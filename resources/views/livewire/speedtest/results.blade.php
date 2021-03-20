3cab27965782a25ccde8ab58e2437b21c43facfe
itsmesid <2363144-itsmesid@users.noreply.gitlab.com>
2021-03-20 14:06:07 +0000

    added menu and routes

--- resources/views/livewire/speedtest/results.blade.php
+++ resources/views/livewire/speedtest/results.blade.php
@@ -1,3 +1,13 @@
-<div>
-    {{-- A good traveler has no fixed plans and is not intent upon arriving. --}}
+<div class="py-12 ">
+    <!-- dark:bg-gray-800 -->
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Speedtest History') }}
+        </h2>
+    </x-slot>
+    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 ">
+
+
+    </div>
+
 </div>
