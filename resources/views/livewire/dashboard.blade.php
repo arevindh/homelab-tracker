18c3873edefbaec3417271eb0cce48cc1b7cdf6a
Vivek <vivekprajeev@gmail.com>
2021-03-20 19:48:31 +0530

    modal popup on clicking element ingraph

--- resources/views/livewire/dashboard.blade.php
+++ resources/views/livewire/dashboard.blade.php
@@ -90,6 +90,28 @@
             <div id="chart" width="400" height="400"></div>
         </div>
 
+      
+
+        <div class="modal h-screen w-full fixed left-0 top-0 flex justify-center items-center bg-black bg-opacity-50 hidden">
+          <!-- modal -->
+          <div class="bg-white rounded shadow-lg w-10/12 md:w-1/3">
+            <!-- modal header -->
+            <div class="border-b px-4 py-2 flex justify-between items-center">
+              <h3 class="font-semibold text-lg">Modal Title</h3>
+              <button class="text-black close-modal">&cross;</button>
+            </div>
+            <!-- modal body -->
+            <div class="p-3">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rerum, delectus cumque fugiat nemo ducimus quae deserunt cupiditate sapiente incidunt aut accusantium dolore assumenda vitae similique, exercitationem voluptatum praesentium laboriosam nam.
+            </div>
+            <div class="flex justify-end items-center w-100 border-t p-3">
+              <button class="bg-red-600 hover:bg-red-700 px-3 py-1 rounded text-white mr-1 close-modal">Cancel</button>
+            </div>
+          </div>
+        </div>
+  
+        
+
         <script>
             var latest_data = {
                 upload: [],
@@ -120,6 +142,15 @@
                             height: '300px',
                             zoom: {
                                 autoScaleYaxis: true
+                            },
+                            events:{
+                                markerClick: function(event, chartContext, data) {
+       // open popup here
+                                        const modal = document.querySelector('.modal');
+                                        modal.classList.remove('hidden')
+                                          
+                                        console.log(data);
+                                     }
                             },
                             stacked: false,
                             // background: '#fff'
@@ -180,7 +211,19 @@
 
             loadLatestSpeedtest();
         </script>
+  <script>
+    const modal = document.querySelector('.modal');
 
+    const showModal = document.querySelector('.show-modal');
+    const closeModal = document.querySelectorAll('.close-modal');
+
+   
+    closeModal.forEach(close => {
+      close.addEventListener('click', function (){
+        modal.classList.add('hidden')
+      });
+    });
+  </script>
 
     </div>
 
