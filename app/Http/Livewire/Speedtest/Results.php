8ed75eac7a0bc8a3785941e320bc9b42e5374b2e
itsmesid <2363144-itsmesid@users.noreply.gitlab.com>
2021-03-20 13:58:12 +0000

    addded results blade

---
+++ app/Http/Livewire/Speedtest/Results.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Speedtest;
+
+use Livewire\Component;
+
+class Results extends Component
+{
+    public function render()
+    {
+        return view('livewire.speedtest.results');
+    }
+}
