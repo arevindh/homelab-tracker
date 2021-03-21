2497dba19258073c1a2fb2636fbc62d7c3e6e334
Vivek <vivekprajeev@gmail.com>
2021-03-21 16:04:33 +0530

    UI change

--- resources/views/livewire/settings.blade.php
+++ resources/views/livewire/settings.blade.php
@@ -9,161 +9,68 @@
         <div class="bg-white rounded">
             <nav class="flex flex-col sm:flex-row">
                 <button class="text-gray-600 py-4 px-6 block hover:text-blue-500 focus:outline-none text-blue-500 border-b-2 font-medium border-blue-500">
-                    Tab 1
+                    General
                 </button>
                 <button class="text-gray-600 py-4 px-6 block hover:text-blue-500 focus:outline-none">
-                    Tab 2
+                    Graphs
                 </button>
                 <button class="text-gray-600 py-4 px-6 block hover:text-blue-500 focus:outline-none">
-                    Tab 3
+                   Notifications
                 </button>
                 <button class="text-gray-600 py-4 px-6 block hover:text-blue-500 focus:outline-none">
-                    Tab 4
+                    Healthchecks.io
+                </button>
+                <button class="text-gray-600 py-4 px-6 block hover:text-blue-500 focus:outline-none">
+                    Reset
+                </button>
+                <button class="text-gray-600 py-4 px-6 block hover:text-blue-500 focus:outline-none">
+                   Backup/Restore
                 </button>
             </nav>
         </div>
         <div class="mt-5 md:mt-0 md:col-span-5">
-            <form action="#" method="POST">
-                <div class="shadow sm:rounded-md sm:overflow-hidden">
-                    <div class="px-4 py-5 bg-white space-y-6 sm:p-6">
-                        <div class="grid grid-cols-3 gap-6">
-                            <div class="col-span-6 sm:col-span-6">
-                                <label for="company_website" class="block text-sm font-medium text-gray-700">
-                                    Website
-                                </label>
-                                <div class="mt-1 flex rounded-md shadow-sm col-span-6">
-                                    <span class="inline-flex items-center px-3 rounded-l-md border border-r-0 border-gray-300 bg-gray-50 text-gray-500 text-sm">
-                                        http://
-                                    </span>
-                                    <input type="text" name="company_website" id="company_website" class="focus:ring-indigo-500 focus:border-indigo-500 flex-1 block w-full rounded-none rounded-r-md sm:text-sm border-gray-300" placeholder="www.example.com">
-                                </div>
-                            </div>
-                        </div>
-
-                        <div>
-                            <label for="about" class="block text-sm font-medium text-gray-700">
-                                About
-                            </label>
-                            <div class="mt-1">
-                                <textarea id="about" name="about" rows="3" class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 mt-1 block w-full sm:text-sm border-gray-300 rounded-md" placeholder="you@example.com"></textarea>
-                            </div>
-                            <p class="mt-2 text-sm text-gray-500">
-                                Brief description for your profile. URLs are hyperlinked.
-                            </p>
-                        </div>
-
-                        <div>
-                            <label class="block text-sm font-medium text-gray-700">
-                                Photo
-                            </label>
-                            <div class="mt-1 flex items-center">
-                                <span class="inline-block h-12 w-12 rounded-full overflow-hidden bg-gray-100">
-                                    <svg class="h-full w-full text-gray-300" fill="currentColor" viewBox="0 0 24 24">
-                                        <path d="M24 20.993V24H0v-2.996A14.977 14.977 0 0112.004 15c4.904 0 9.26 2.354 11.996 5.993zM16.002 8.999a4 4 0 11-8 0 4 4 0 018 0z" />
-                                    </svg>
-                                </span>
-                                <button type="button" class="ml-5 bg-white py-2 px-3 border border-gray-300 rounded-md shadow-sm text-sm leading-4 font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                                    Change
-                                </button>
-                            </div>
-                        </div>
-
-                        <div>
-                            <label class="block text-sm font-medium text-gray-700">
-                                Cover photo
-                            </label>
-                            <div class="mt-1 flex justify-center px-6 pt-5 pb-6 border-2 border-gray-300 border-dashed rounded-md">
-                                <div class="space-y-1 text-center">
-                                    <svg class="mx-auto h-12 w-12 text-gray-400" stroke="currentColor" fill="none" viewBox="0 0 48 48" aria-hidden="true">
-                                        <path d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-                                    </svg>
-                                    <div class="flex text-sm text-gray-600">
-                                        <label for="file-upload" class="relative cursor-pointer bg-white rounded-md font-medium text-indigo-600 hover:text-indigo-500 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500">
-                                            <span>Upload a file</span>
-                                            <input id="file-upload" name="file-upload" type="file" class="sr-only">
-                                        </label>
-                                        <p class="pl-1">or drag and drop</p>
-                                    </div>
-                                    <p class="text-xs text-gray-500">
-                                        PNG, JPG, GIF up to 10MB
-                                    </p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="px-4 py-3 bg-gray-50 text-right sm:px-6">
-                        <button type="submit" class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                            Save
-                        </button>
-                    </div>
-                </div>
-            </form>
+           
         </div>
 
-        <div class="hidden sm:block" aria-hidden="true">
-            <div class="py-5">
-                <div class="border-t border-gray-200"></div>
-            </div>
-        </div>
-
+    {{-- General section form area --}}
         <div class="mt-10 sm:mt-0">
-
-
             <div class="mt-5 md:mt-0 md:col-span-5">
                 <form action="#" method="POST">
                     <div class="shadow overflow-hidden sm:rounded-md">
                         <div class="px-4 py-5 bg-white sm:p-6">
                             <div class="grid grid-cols-6 gap-6">
-                                <div class="col-span-6 sm:col-span-3">
-                                    <label for="first_name" class="block text-sm font-medium text-gray-700">First
-                                        name</label>
-                                    <input type="text" name="first_name" id="first_name" autocomplete="given-name" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">
+                                <div class="col-span-12 sm:col-span-12">
+                                    <label for="first_name" class="block text-sm font-medium text-gray-700">App Name (Set a custom name for you application)</label>
+                                    <input type="text" name="app_name" id="app_name" autocomplete="app_name" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">
+                                </div>
+                                <div class="col-span-6 sm:col-span-6">
+                                    <label for="schedule_enable" class="block text-sm font-medium text-gray-700">Enable Schedule (Enable or Diasble the schedule worker)</label>
+                                    <input type="checkbox" name="schedule_enable" id="schedule_enable" autocomplete="family-name" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 shadow-sm sm:text-sm border-gray-300 rounded-md">
                                 </div>
 
-                                <div class="col-span-6 sm:col-span-3">
-                                    <label for="last_name" class="block text-sm font-medium text-gray-700">Last
-                                        name</label>
-                                    <input type="text" name="last_name" id="last_name" autocomplete="family-name" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">
+                                <div class="col-span-6 sm:col-span-6">
+                                    <label for="schedule" class="block text-sm font-medium text-gray-700 ">Schedule</label>
+                                    <input type="text" name="schedule" id="schedule" autocomplete="family-name" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">
                                 </div>
 
-                                <div class="col-span-6 sm:col-span-4">
-                                    <label for="email_address" class="block text-sm font-medium text-gray-700">Email
-                                        address</label>
-                                    <input type="text" name="email_address" id="email_address" autocomplete="email" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">
+                                <div class="col-span-12 sm:col-span-12">
+                                    <label for="server" class="block text-sm font-medium text-gray-700">Server</label>
+                                    <input type="text" name="server" id="server" autocomplete="server" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">
                                 </div>
 
-                                <div class="col-span-6 sm:col-span-3">
-                                    <label for="country" class="block text-sm font-medium text-gray-700">Country /
-                                        Region</label>
-                                    <select id="country" name="country" autocomplete="country" class="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
-                                        <option>United States</option>
-                                        <option>Canada</option>
-                                        <option>Mexico</option>
-                                    </select>
+                                <div class="col-span-12 sm:col-span-6">
+                                    <label for="schedule_enable" class="block text-sm font-medium text-gray-700">Show Average (If enabled, the average value for speedtest will be shown in the widget)</label>
+                                    <input type="checkbox" name="schedule_enable" id="schedule_enable" autocomplete="family-name" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 shadow-sm sm:text-sm border-gray-300 rounded-md">
                                 </div>
-
-                                <div class="col-span-6">
-                                    <label for="street_address" class="block text-sm font-medium text-gray-700">Street
-                                        address</label>
-                                    <input type="text" name="street_address" id="street_address" autocomplete="street-address" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">
+                                <div class="col-span-12 sm:col-span-6">
+                                    <label for="schedule_enable" class="block text-sm font-medium text-gray-700">Show Max (If enabled, the maximum value for speedtest will be shown in the widget)</label>
+                                    <input type="checkbox" name="schedule_enable" id="schedule_enable" autocomplete="family-name" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 shadow-sm sm:text-sm border-gray-300 rounded-md">
                                 </div>
-
-                                <div class="col-span-6 sm:col-span-6 lg:col-span-2">
-                                    <label for="city" class="block text-sm font-medium text-gray-700">City</label>
-                                    <input type="text" name="city" id="city" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">
+                                <div class="col-span-12 sm:col-span-6">
+                                    <label for="schedule_enable" class="block text-sm font-medium text-gray-700">Show Min (If enabled, the minimum value for speedtest will be shown in the widget)</label>
+                                    <input type="checkbox" name="schedule_enable" id="schedule_enable" autocomplete="family-name" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 shadow-sm sm:text-sm border-gray-300 rounded-md">
                                 </div>
-
-                                <div class="col-span-6 sm:col-span-3 lg:col-span-2">
-                                    <label for="state" class="block text-sm font-medium text-gray-700">State /
-                                        Province</label>
-                                    <input type="text" name="state" id="state" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">
-                                </div>
-
-                                <div class="col-span-6 sm:col-span-3 lg:col-span-2">
-                                    <label for="postal_code" class="block text-sm font-medium text-gray-700">ZIP /
-                                        Postal</label>
-                                    <input type="text" name="postal_code" id="postal_code" autocomplete="postal-code" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">
-                                </div>
+                               
                             </div>
                         </div>
                         <div class="px-4 py-3 bg-gray-50 text-right sm:px-6">
@@ -176,6 +83,7 @@
             </div>
 
         </div>
+    {{-- General section form area end --}}
     </div>
 
 </div>
