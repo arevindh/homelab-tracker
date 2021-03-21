ce2a3ad7827b695641f13c7f8b7e39cc509a58f8
Vivek <vivekprajeev@gmail.com>
2021-03-21 16:06:11 +0530

    text changes

--- resources/views/livewire/settings.blade.php
+++ resources/views/livewire/settings.blade.php
@@ -54,7 +54,7 @@
                                 </div>
 
                                 <div class="col-span-12 sm:col-span-12">
-                                    <label for="server" class="block text-sm font-medium text-gray-700">Server</label>
+                                    <label for="server" class="block text-sm font-medium text-gray-700">Server (Sets the schedule for the speedtests to run using the CRON format)</label>
                                     <input type="text" name="server" id="server" autocomplete="server" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">
                                 </div>
 
