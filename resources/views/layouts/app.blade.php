075adc77a0ca21347d355f0b03baef9f64fe4e2b
itsmesid <2363144-itsmesid@users.noreply.gitlab.com>
2021-03-22 04:52:49 +0000

    footer position changed

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -139,32 +139,30 @@
         <main>
             {{ $slot }}
         </main>
-    </div>
 
-
-    <div class="bg-gray-100 pt-2">
-        <div class="flex pb-8 px-3 m-auto pt-5 border-t text-gray-800 text-sm flex-col md:flex-row max-w-7xl">
-            <div class="mt-2"> <a href="https://github.com/arevindh/homelab-tracker" target="_blank">HomelabTracker</a> </div>
-            <div class="md:flex-auto md:flex-row-reverse mt-2 flex-row flex">
-                <a href="/#" class="w-6 mx-1">
-                    x
-                </a>
-                <a href="/#" class="w-6 mx-1">
-                   y
-                </a>
-                <a href="/#" class="w-6 mx-1">
-                    z
-                </a>
-                <a href="/#" class="w-6 mx-1">
-                   a
-                </a>
-                <a href="/#" class="w-6 mx-1">
-                   b
-                </a>
+        <div class="bg-gray-100 pt-2" id="footer">
+            <div class="flex pb-8 px-3 m-auto pt-5 border-t text-gray-800 text-sm flex-col md:flex-row max-w-7xl">
+                <div class="mt-2"> <a href="https://github.com/arevindh/homelab-tracker" target="_blank">HomelabTracker</a> </div>
+                <div class="md:flex-auto md:flex-row-reverse mt-2 flex-row flex">
+                    <a href="/#" class="w-6 mx-1">
+                        x
+                    </a>
+                    <a href="/#" class="w-6 mx-1">
+                        y
+                    </a>
+                    <a href="/#" class="w-6 mx-1">
+                        z
+                    </a>
+                    <a href="/#" class="w-6 mx-1">
+                        a
+                    </a>
+                    <a href="/#" class="w-6 mx-1">
+                        b
+                    </a>
+                </div>
             </div>
         </div>
     </div>
-
     @stack('modals')
 
     @livewireScripts
