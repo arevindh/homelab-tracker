89780de8154049b5815ceafbf99622910021d173
itsmesid <2363144-itsmesid@users.noreply.gitlab.com>
2021-03-22 08:44:38 +0000

    move to seperate component

--- app/Http/Livewire/Widgets/Statistics.php
+++ app/Http/Livewire/Widgets/Statistics.php
@@ -1,43 +1,26 @@
 <?php
 
-namespace App\Http\Livewire;
+namespace App\Http\Livewire\Widgets;
 
-use App\Jobs\SpeedtestJob;
 use App\Models\Speedtest;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\DB;
 use Livewire\Component;
-use Illuminate\Support\Facades\Cache;
 
-class Dashboard extends Component
+class Statistics extends Component
 {
-    public $latest;
-    public $stats;
-
-    protected $listeners = ['runSpeedtest' => 'testSpeed'];
-
-    public function mount()
-    {
-        
-        $this->latest = Speedtest::where('status', 'success')->select('id', 'timestamp', 'isp', 'ping_jitter', 'ping_latency', 'download_bandwidth', 'upload_bandwidth')->latest()->first();
-    }
-
     public function render()
     {
 
-        $this->stats = Cache::remember('speedtests', 60, function () {
+        $stats = Cache::remember('speedtests', 60, function () {
             return Speedtest::where('status', 'success')
                 ->select('id', 'name', DB::raw('round(AVG(ping_latency),0) as avg_ping_latency, round(AVG(ping_jitter),0) as avg_ping_jitter , round(AVG(download_bandwidth),0) as avg_download_bandwidth , round(AVG(upload_bandwidth),0) as avg_upload_bandwidth, round(max(ping_latency),0) as max_ping_latency, round(max(ping_jitter),0) as max_ping_jitter , round(max(download_bandwidth),0) as max_download_bandwidth , round(max(upload_bandwidth),0) as max_upload_bandwidth, round(min(ping_latency),0) as min_ping_latency, round(min(ping_jitter),0) as min_ping_jitter , round(min(download_bandwidth),0) as min_download_bandwidth , round(min(upload_bandwidth),0) as min_upload_bandwidth'))
                 ->first();
         });
 
-        
+        $latest = Speedtest::where('status', 'success')->select('id', 'timestamp', 'isp', 'ping_jitter', 'ping_latency', 'download_bandwidth', 'upload_bandwidth')->latest()->first();
 
-        return view('livewire.dashboard');
-    }
 
-    public function testSpeed()
-    {
-        SpeedtestJob::dispatch();
-        $this->dispatchBrowserEvent('reloadGraph');
+        return view('livewire.widgets.statistics', ['stats' => $stats, 'latest' => $latest]);
     }
 }
--- resources/views/livewire/dashboard.blade.php
+++ resources/views/livewire/dashboard.blade.php
@@ -6,93 +6,17 @@
         </h2>
     </x-slot>
     <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-        <div class="flex flex-col lg:flex-row w-full lg:space-x-2 space-y-2 lg:space-y-0 mb-2 lg:mb-4" >
 
-            <div class="w-full lg:w-1/4">
-                <div class="widget w-full p-4 rounded-lg bg-white border-l-4 border-green-400">
-                    <div class="flex items-center">
-                        <div class="icon w-14  rounded-full mr-3">
-                            <span class="material-icons" style="color: #39B29D;
-    font-size: 58px;">
-                                cloud_download
-                            </span>
-                        </div>
-                        <div class="flex flex-col justify-center">
-                            <div class="text-lg font-black">{{convertToReadableSize($latest->download_bandwidth)}}</div>
-                            <div class="text-xs">Max : {{convertToReadableSize($stats->max_download_bandwidth)}} </div>
-                            <div class="text-xs">Min : {{convertToReadableSize($stats->min_download_bandwidth)}}</div>
-                            <div class="text-xs">Avg : {{convertToReadableSize($stats->avg_download_bandwidth)}}</div>
-                            <div class="text-sm text-gray-400">Download Speed</div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <div class="w-full lg:w-1/4">
-                <div class="widget w-full p-4 rounded-lg bg-white border-l-4 border-yellow-400">
-                    <div class="flex items-center">
-                        <div class="icon w-14   text-white rounded-full mr-3">
-                            <span class="material-icons" style="color: #ff932f;
-                            font-size: 58px;">
-                                cloud_upload
-                            </span>
-                        </div>
-                        <div class="flex flex-col justify-center">
-                            <div class="text-lg font-black">{{convertToReadableSize($latest->upload_bandwidth)}}</div>
-                            <div class="text-xs">Max {{convertToReadableSize($stats->max_upload_bandwidth)}} </div>
-                            <div class="text-xs">Min {{convertToReadableSize($stats->min_upload_bandwidth)}}</div>
-                            <div class="text-xs">Avg: {{convertToReadableSize($stats->avg_upload_bandwidth)}}</div>
-                            <div class="text-sm text-gray-400">Upload Speed</div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <div class="w-full lg:w-1/4">
-                <div class="widget w-full p-4 rounded-lg bg-white border-l-4 border-red-400">
-                    <div class="flex items-center">
-                        <div class="icon w-14   text-white rounded-full mr-3">
-                            <span class="material-icons" style="color: red;
-                            font-size: 58px;">
-                                network_check
-                            </span>
-                        </div>
-                        <div class="flex flex-col justify-center">
-                            <div class="text-lg font-black">{{$latest->ping_latency}} ms</div>
-                            <div class="text-xs">Max : {{$stats->max_ping_latency}} ms</div>
-                            <div class="text-xs">Min : {{$stats->min_ping_latency}} ms</div>
-                            <div class="text-xs">Avg : {{$stats->avg_ping_latency}} ms</div>
-                            <div class="text-sm text-gray-400">Latency</div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <div class="w-full lg:w-1/4">
-                <div class="widget w-full p-4 rounded-lg bg-white border-l-4 border-blue-400">
-                    <div class="flex items-center">
-                        <div class="icon w-14   text-white rounded-full mr-3">
-                            <span class="material-icons" style="color: blue;
-                            font-size: 58px;">
-                                cable
-                            </span>
-                        </div>
-                        <div class="flex flex-col justify-center">
-                            <div class="text-lg font-black">{{convertToReadableSize($latest->ping_jitter)}}</div>
-                            <div class="text-sm">Max : {{$stats->max_ping_jitter}} ms</div>
-                            <div class="text-xs">Min : {{$stats->min_ping_jitter}} ms</div>
-                            <div class="text-xs">Avg : {{$stats->avg_ping_jitter}} </div>
-                            <div class="text-sm text-gray-400">Jitter</div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+        <livewire:widgets.statistics />
 
         <div class="px-4 py-5 bg-white space-y-6 sm:p-6 ">
             <!-- dark:bg-gray-800 -->
             <div id="chart" width="400" height="400"></div>
         </div>
+
+        <button wire:click="$emit('runSpeedtest')">
+            Test Speed
+        </button>
 
 
 
@@ -286,9 +210,11 @@
                     }
 
                     var chart = new ApexCharts(document.querySelector('#chart'), options)
-                    chart.render()
+                    chart.render();
 
-
+                    window.addEventListener('reloadGraph', event => {
+                        chart.render()
+                    })
                 });
             }
 
@@ -307,6 +233,9 @@
                 });
             });
         </script>
+
+
+
 
     </div>
 
